266064c8fece2efccbbfea935c6d3fd51c7f7c05
FES001 <firdavs7794948@gmail.com>
2024-11-27 21:10:39 +0500

    Working on it.

---
+++ scripts/display-data.js
@@ -0,0 +1,33 @@
+const challengeNameText = document.querySelector('.challenge-header-itself');
+const challengeShortDescriptionText = document.querySelector('.challenge-header-paragraph');
+const challengeDetailedDescriptionText = document.querySelector('.challenge-detailed-info-paragraph');
+const challengeDifficultyText = document.getElementById('challengeDifficultyText');
+const challengeGithubLink = document.getElementById('githubLink');
+const challengeLiveLink = document.getElementById('liveLink');
+const challengeImageItself = document.querySelector('.challenge-image-itself');
+
+
+// ACCESSING THE DATA FROM LOCAL STORAGE
+
+function getDataFromLocalStorage() {
+    const challengePicture = localStorage.getItem('challengePicture');
+    const challengeName = localStorage.getItem('challengeName');
+    const challengeShortDescription = localStorage.getItem('challengeShortDescription');
+    const challengeDetailedDescription = localStorage.getItem('challengeDetailedDescription');
+    const challengeDifficulty = localStorage.getItem('challengeDifficulty');
+    const challengeGitHub = localStorage.getItem('challengeGitHub');
+    const challengeliveWebsite = localStorage.getItem('challengeliveWebsite');
+
+    if (challengePicture) {
+        challengeImageItself.src = challengePicture;
+        challengeNameText.textContent = challengeName;
+        challengeShortDescriptionText.textContent = challengeShortDescription;
+        challengeDetailedDescriptionText.textContent = challengeDetailedDescription;
+        challengeDifficultyText.textContent = challengeDifficulty;
+        challengeGithubLink.href = challengeGitHub;
+        challengeLiveLink.href = challengeliveWebsite;
+    };
+};
+
+
+getDataFromLocalStorage();
